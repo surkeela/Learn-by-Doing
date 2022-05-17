a909ac61c71373913a9c1ad4093f1bf1e5c1853c
Nerijus Šurkys <46633513+surkeela@users.noreply.github.com>
2022-05-17 18:03:57 +0300

    Initial Commit

---
+++ Learn by Doing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by Nerijus Surkys on 2022-05-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
