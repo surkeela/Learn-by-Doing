3f81bc47af8a9da981431b2fbf7d93004a0bdbb4
Nerijus Šurkys <46633513+surkeela@users.noreply.github.com>
2022-05-21 16:17:39 +0300

    WP
    reapeating card view

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -12,7 +12,14 @@ struct ContentView: View {
     // MARK: - CONTENT
     
     var body: some View {
-        CardView()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 20) {
+                ForEach(0 ..< 6) { item in
+                    CardView()
+                }
+            }
+            .padding()
+        }
     }
 }
 
